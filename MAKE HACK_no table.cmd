1e1499ca52151b151c7d5b2eae1812df1c3b6e10
MysticOCE <nykhan9@gmail.com>
2017-12-29 14:01:39 +1100

    gonna fix up wm
    Last update before I try to mess with Castle Renais and the WM stuff to get the route split stuff working

---
+++ MAKE HACK_no table.cmd
@@ -0,0 +1,17 @@
+﻿cd %~dp0
+
+copy FE8_clean.gba REDFE.gba
+
+cd "%~dp0Text"
+
+textprocess_v2 textbuildfile.txt
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0REDFE.gba" "-input:%~dp0ROM Buildfile.event"
+
+cd "%~dp0ups"
+
+ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0REDFE.gba" -o "%~dp0REDFE.ups"
+
+pause
