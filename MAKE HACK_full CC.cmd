1007115cb779ae716e3092dd2e2399e3ffe40df4
MysticOCE <nykhan9@gmail.com>
2018-01-20 22:41:58 +1100

    Roy1 complete
    Roy 1 is pretty much complete, still needs a playtest but it functions well
    
    WM stuff is functionally done, plenty of eventing/text to be done to restore story

---
+++ MAKE HACK_full CC.cmd
@@ -0,0 +1,21 @@
+﻿cd %~dp0
+
+copy FE8_clean.gba REDFE.gba
+
+cd "%~dp0Tables"
+
+c2ea "%~dp0FE8_clean.gba"
+
+cd "%~dp0Text"
+
+textprocess_v2 textbuildfile.txt
+
+cd "%~dp0Event Assembler"
+
+ColorzCore A FE8 "-output:%~dp0REDFE.gba" "-input:%~dp0ROM Buildfile.event"
+
+cd "%~dp0ups"
+
+ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0REDFE.gba" -o "%~dp0REDFE.ups"
+
+pause
