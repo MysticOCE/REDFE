516318072b0fc2d816ecc4a2bbe49adfff3dea41
MysticOCE <nykhan9@gmail.com>
2020-07-14 18:03:28 +1000

    r6 map
    map done and pretty clean, ready for test

---
+++ MiscGraphic/Desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Desert" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="GammaDesert/6800696A.png" width="512" height="512"/>
+</tileset>
