688a3199a9d058e2b15d0855f2a054554a6326f1
MysticOCE <nykhan9@gmail.com>
2020-08-25 00:54:12 +1000

    title done

--- MiscGraphic/LeoCG/titlebackgroundimage.h
+++ MiscGraphic/LeoCG/titlebackgroundimage.h
@@ -8,7 +8,7 @@
 //	+ 600 tiles not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2020-08-24, 23:04:29
+//	Time-stamp: 2020-08-24, 23:44:52
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
