2602fe3dcd7a5f30c9aff24414ec560e92811812
MysticOCE <nykhan9@gmail.com>
2020-08-27 18:52:23 +1000

    title screen
    kept one sprite I want

---
+++ MiscGraphic/LeoCG/titlebackgroundinstaller.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(titlebackgroundinstaller)
+
+//======================================================================
+//
+//	titlebackgroundinstaller, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 600 tiles not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2020-08-27, 17:15:08
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TITLEBACKGROUNDINSTALLER_H
+#define GRIT_TITLEBACKGROUNDINSTALLER_H
+
+#define titlebackgroundinstallerTilesLen 38400
+extern const unsigned short titlebackgroundinstallerTiles[19200];
+
+#define titlebackgroundinstallerPalLen 512
+extern const unsigned short titlebackgroundinstallerPal[256];
+
+#endif // GRIT_TITLEBACKGROUNDINSTALLER_H
+
+//}}BLOCK(titlebackgroundinstaller)
