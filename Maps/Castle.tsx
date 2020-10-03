63a87abfc99d78f70c7d602040983d63cbb18067
MysticOCE <nykhan9@gmail.com>
2020-10-03 12:07:14 +1000

    wm change
    going from r6 to renais now

--- Maps/Castle.tsx
+++ Maps/Castle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="Castle" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../../../Tools/FEMapCreator/Tilesets/FE8 - Castle - 1800481a.png" width="512" height="512"/>
+ <image source="../../../../Tools/FEXNA Beta/FEMapCreator/Tilesets/FE8 - Castle - 1800481a.png" width="512" height="512"/>
 </tileset>
